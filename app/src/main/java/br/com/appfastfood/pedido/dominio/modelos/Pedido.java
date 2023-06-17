cace6b8252eafc1a697a7dc809cec81dc0894071
caikegburgos <caikegabriel1997@gmail.com>
2023-06-17 01:00:53 -0300

    Classe pedido e enum de status do pedido

--- app/src/main/java/br/com/appfastfood/pedido/dominio/modelos/Pedido.java
+++ app/src/main/java/br/com/appfastfood/pedido/dominio/modelos/Pedido.java
@@ -1,5 +1,46 @@
 package br.com.appfastfood.pedido.dominio.modelos;
- 
+import java.math.BigDecimal;
+import br.com.appfastfood.cliente.dominio.modelos.Cliente;
+import br.com.appfastfood.pedido.dominio.modelos.enums.StatusPedidoEnum;
+import br.com.appfastfood.produto.dominio.modelos.Produto;
+
 public class Pedido { 
-    // Atributos, construtor e métodos... 
+    private Produto produto;
+    private Long quantidade;
+    private Cliente cliente;
+    private BigDecimal valorTotal;
+    private StatusPedidoEnum status;
+
+    public Pedido(Produto produto, Long quantidade, Cliente cliente, BigDecimal valorTotal, StatusPedidoEnum status){
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.cliente = cliente;
+        this.valorTotal = valorTotal;
+        this.status = status;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public Long getQuantidade() {
+        return quantidade;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public StatusPedidoEnum getStatus() {
+        return status;
+    }
+
+    public void atualizarValorTotalPedido(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
 } 
---
+++ app/src/main/java/br/com/appfastfood/pedido/dominio/modelos/enums/StatusPedidoEnum.java
@@ -0,0 +1,8 @@
+package br.com.appfastfood.pedido.dominio.modelos.enums;
+
+public enum StatusPedidoEnum {
+    recebido,
+    emPreparacao,
+    pronto,
+    finalizado
+}
